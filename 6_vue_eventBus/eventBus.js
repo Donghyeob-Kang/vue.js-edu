d344b8308eabe193516c68ed08717d757de11fcc
vassili <falcon7459@gmail.com>
2020-02-10 17:07:59 +0900

    eventBus

---
+++ 6_vue_eventBus/eventBus.js
@@ -0,0 +1,5 @@
+// eventBus는 event를 중앙관리 하자는 개념
+// eventBus의 단점은 root component가 난잡해짐
+import Vue from 'vue';
+
+export default new Vue();
--- 6_vue_eventBus/ticTacToe.vue
+++ 6_vue_eventBus/ticTacToe.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
         <div>{{ turn }}님의 턴입니다.</div>
-        <!-- tableData를 tableComponent로 상속 -->
         <table-component :table-data="tableData" />
         <div v-if="winner">{{ winner }}님의 승리!</div>
     </div>
 </template>
 
 <script>
-    import tableComponent from './tableComponent.vue';
+    import tableComponent from "./tableComponent.vue";
+    import eventBus from "./eventBus.js";
 
     export default {
         components: {
@@ -17,15 +17,82 @@
         data() {
             return {
                 tableData: [
-                    ['', '', ''],
-                    ['', '', ''],
-                    ['', '', '']
+                    ["", "", ""],
+                    ["", "", ""],
+                    ["", "", ""]
                 ],
-                turn: 'O',
-                winner: ''
+                turn: "O",
+                winner: ""
             };
         },
-        methods: {}
+        methods: {
+            onClickTd(rowIndex, cellIndex) {
+                console.log(rowIndex, cellIndex);
+                this.$set(this.tableData[rowIndex], cellIndex, this.turn);
+                let win = false;
+                if (
+                    this.tableData[rowIndex][0] === this.turn &&
+                    this.tableData[rowIndex][1] === this.turn &&
+                    this.tableData[rowIndex][2] === this.turn
+                ) {
+                    win = true;
+                }
+                if (
+                    this.tableData[0][cellIndex] === this.turn &&
+                    this.tableData[1][cellIndex] === this.turn &&
+                    this.tableData[2][cellIndex] === this.turn
+                ) {
+                    win = true;
+                }
+                if (
+                    this.tableData[0][0] === this.turn &&
+                    this.tableData[1][1] === this.turn &&
+                    this.tableData[2][2] === this.turn
+                ) {
+                    win = true;
+                }
+                if (
+                    this.tableData[0][2] === this.turn &&
+                    this.tableData[1][1] === this.turn &&
+                    this.tableData[2][0] === this.turn
+                ) {
+                    win = true;
+                }
+
+                if (win) {
+                    this.winner = this.turn;
+                    this.turn = "O";
+                    this.tableData = [
+                        ["", "", ""],
+                        ["", "", ""],
+                        ["", "", ""]
+                    ];
+                } else {
+                    let all = true;
+                    this.tableData.forEach(row => {
+                        row.forEach(cell => {
+                            if (!cell) {
+                                all = false;
+                            }
+                        });
+                    });
+                    if (all) {
+                        this.winner = "";
+                        this.turn = "O";
+                        this.tableData = [
+                            ["", "", ""],
+                            ["", "", ""],
+                            ["", "", ""]
+                        ];
+                    } else {
+                        this.turn = this.turn === "O" ? "X" : "O";
+                    }
+                }
+            }
+        },
+        created() {
+            eventBus.$on("clickTd", this.onClickTd);
+        }
     };
 </script>
 
